01fb59706752e62a37c58d16bda3f3e362b3ea1c
Jesse Yuen <contactjesseyuen@gmail.com>
2022-04-22 17:27:06 +0100

    Friday arvo commit - setting up some templates and stuff. Making a start on the complaints flow.

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+//Frot door
+router.post('/contact-type-answer', function (req, res) {
+
+let contactTypeChoice = req.session.data['contact-type']
+
+if (contactTypeChoice === 'complaint') {
+    res.redirect('/complaints')
+} else {
+    //res.redirect('/job_alerts2/create-success-email')
+}
+
+})
+
 module.exports = router
